79f24ba97ddd93a710151f8f2afd8dbdc9cdad77
Dharmik Gohil <dharmikg86@gmail.com>
2023-02-06 23:25:02 +0530

    initial commit

---
+++ js/readmore.js
@@ -0,0 +1,42 @@
+const btns = document.querySelectorAll(".myBtn");
+
+btns.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    var dots = document.getElementById("dots" + id);
+    var moreText = document.getElementById("more" + id);
+    var btnText = document.getElementById("myBtn" + id);
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  });
+});
+
+// tab component
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
